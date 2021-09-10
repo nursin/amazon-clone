2a6ef444016a0dfd77e7ff367c920d835737b737
Bobby Keel <bobbykeel76@gmail.com>
2021-09-09 22:47:55 -0500

    add checkoutProduct to checkout page, can remove individual items from checkout page

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import '../Checkout.css';
+import CheckoutProduct from './CheckoutProduct';
 import Subtotal from './Subtotal';
+import { useStateValue } from '../redux/StateProvider';
+
 
 function Checkout() {
+
+  const [{basket}, dispatch] = useStateValue();
+
   return (
     <div className='checkout'>
       <div className='checkout_left'>
         <img className='checkout_ad' src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg' alt=''/>
         <div>
           <h2 className='checkout__title'>Your shopping basket</h2>
+          {basket.map(item =>(
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
       <div className='checkout_right'>
