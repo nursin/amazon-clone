584bbf85a3a86e670d9af1b509bc4fd05a15d620
Bobby Keel <bobbykeel76@gmail.com>
2021-09-10 17:05:12 -0500

    firebase functions backend build, installed cors express stripe, imports

---
+++ functions/index.js
@@ -0,0 +1,22 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors')
+const stripe = rewuire('stripe')('sk_test_51JYEXdBXcG2jvy6vDpFcVpJAygDUYW6gySynJx9rnJI9LeTI04pFhwPCwe4w8uRx1Z3PEuJ4z7Fo3KNVi2zLfFrg00B0HE7u6f');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+// API
+
+// - App config
+
+// - Middlewares
+
+// - API routes
+
+// - Listen commands
